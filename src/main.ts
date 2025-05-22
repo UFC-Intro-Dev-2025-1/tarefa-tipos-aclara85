5296c16c0cfc56d8fcb9f1afad239334d97ec033
aclara85 <antonia.clara.oliveira808@gmail.com>
2025-05-22 23:56:11 +0000

    tarefa tipo

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-// Escreva seu código aqui!
+// Tipos number e string
+let gabinete: number = 25;
+let periferico: string = 'Mouse';
+let projetorLigado: boolean = true;
+let quadroBranco: undefined = undefined;
+let professor: { nome: string; disciplina: string } = {
+  nome: 'lana',
+  disciplina: 'fup'
+};
+console.log('Gabinete:', gabinete);
+console.log('Periférico:', periferico);
+console.log('Projetor ligado?', projetorLigado);
+console.log('Quadro branco:', quadroBranco);
+console.log('Professor:', professor);
